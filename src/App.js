961d906543761c51efa2d91c560873ed544245ac
Zac Braddy <Zac Braddy>
2017-06-06 22:57:03 +0100

    Reverting from develop branch to the end of u2-m2

--- src/App.js
+++ src/App.js
@@ -1,83 +1,18 @@
 import React, { Component } from 'react';
-import Header from './Header/components/Header';
-import IngredientList from './IngredientList/components/IngredientList';
-import RecipeList from './RecipeList/components/RecipeList';
-import liveApi from './liveApi';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    ingredients: [],
-    nextIngredient: '',
-    recipes: [],
-    canSearch: false,
-  };
-
-  addIngredient = () => {
-    if (this.state.nextIngredient.length === 0) return;
-
-    this.setState({
-      ingredients: this.state.ingredients.concat(this.state.nextIngredient),
-      nextIngredient: '',
-      canSearch: true,
-    });
-  };
-
-  removeIngredient = (indexToRemove) => {
-    const newIngredients = this.state.ingredients.concat([]);
-    newIngredients.splice(indexToRemove, 1);
-    const newCanSearch = newIngredients.length !== 0;
-
-    this.setState({
-      ingredients: newIngredients,
-      canSearch: newCanSearch,
-    });
-  };
-
-  handleAddNextChange = (newAddNextIngredient) => {
-    this.setState({
-      nextIngredient: newAddNextIngredient
-    });
-  };
-
-  doSearch = () => {
-    this.setState({
-      canSearch: false,
-    });
-
-    liveApi(this.state.ingredients.join(',')).then((recipes) => {
-      this.setState({
-        recipes,
-        canSearch: true,
-      });
-    });
-  };
-
   render() {
     return (
-      <div className="App container-fluid">
-        <div className="row container-fluid">
-          <Header />
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
         </div>
-        <div className="row content-row">
-          <div className="col-lg-4 ingredients-col">
-            <IngredientList
-              ingredients={this.state.ingredients}
-              nextIngredient={this.state.nextIngredient}
-              addIngredient={this.addIngredient}
-              doSearch={this.doSearch}
-              removeIngredient={this.removeIngredient}
-              handleAddNextChange={this.handleAddNextChange}
-              canSearch={this.state.canSearch}
-            />
-          </div>
-          <div className="col-lg-8 recipes-col">
-            {
-              this.state.recipes.length > 0
-              && <RecipeList recipes={this.state.recipes}/>
-            }
-          </div>
-        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
