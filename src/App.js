5f2b817aab54ac676621f3a072ce4fc6e9274180
Zac Braddy <zac.braddy@buyingbutler.com>
2017-10-19 22:13:48 +0100

    Reset develop back to u2-m2

--- src/App.js
+++ src/App.js
@@ -1,108 +1,11 @@
 import React, { Component } from 'react';
-import Display from './Display';
-import ColouredConsole from './colourConsole';
-
 import './App.css';
 
 class App extends Component {
-  static defaultProps = {
-    helloMessage: `G'day World!`,
-    goodbyeMessage: 'See ya world!',
-    helloButtonText: "Hiya",
-    goodbyeButtonText: "See ya",
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hello: true,
-      visible: true,
-      time: new Date(),
-    };
-
-    this.colouredConsole = new ColouredConsole();
-    this.colouredConsole.setColour('#333', '#ff99c3');
-    this.colouredConsole.log('Display: constructor', ' just fired!');
-  }
-
-  componentWillMount() {
-    this.colouredConsole.log('App: componentWillMount', ' just fired!');
-  }
-
-  componentDidMount() {
-    this.colouredConsole.log('App: componentDidMount', ' just fired!');
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    this.colouredConsole.log('App: shouldComponentUpdate', ` just fired with nextProps set to: ${JSON.stringify(nextProps)}; and nextState set to: ${JSON.stringify(nextState)}`);
-    return true;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    this.colouredConsole.log('App: componentWillUpdate', ` just fired with nextProps set to: ${JSON.stringify(nextProps)}; and nextState set to: ${JSON.stringify(nextState)}`);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    this.colouredConsole.log('App: componentDidUpdate', ` just fired with prevProps set to: ${JSON.stringify(prevProps)}; and prevState set to: ${JSON.stringify(prevState)}`);
-  }
-
-  getMessage = () =>
-    this.state.hello
-      ? this.props.helloMessage
-      : this.props.goodbyeMessage;
-
-  getButtonText = () =>
-    !this.state.hello
-      ? this.props.helloButtonText
-      : this.props.goodbyeButtonText;
-
-  handleMessageButtonClick = () => {
-    this.setState(
-      {
-        hello: !this.state.hello
-      }
-    )
-  };
-
-  handleUpdateTimeClick = () => {
-    this.setState(
-      {
-        time: new Date(),
-      }
-    )
-  };
-
-  handleHideButtonClick = () => {
-    this.setState({
-      visible: !this.state.visible
-    });
-  };
-
   render() {
-    this.colouredConsole.log('App: render', ' just fired!');
     return (
       <div>
-        {
-          this.state.visible &&
-          (<div>
-            <Display message={this.getMessage()}/>
-            <button onClick={this.handleMessageButtonClick}>
-              {this.getButtonText()}
-            </button>
-            <div>
-              {this.state.time.toString()}
-            </div>
-            <button onClick={this.handleUpdateTimeClick}>
-              Update Time
-            </button>
-          </div>)
-        }
-        <div>
-          <button onClick={this.handleHideButtonClick}>
-            { this.state.visible ? 'Hide App' : 'Show App' }
-          </button>
-        </div>
+        Hello World!
       </div>
     );
   }
