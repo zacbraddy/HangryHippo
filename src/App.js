3e485d33d5fd4a05935a02bd0325b24abcf52054
Zac Braddy <zac.braddy@buyingbutler.com>
2017-10-22 01:12:44 +0100

    Checking in code for u4-m1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="list-item-card">
         Hello World!
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders a div', () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper.find('div').length).toBe(1);
 });
