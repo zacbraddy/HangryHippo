b8fd292a6bd5eb77c699610868ec3e4058672830
Zac Braddy <zac.braddy@buyingbutler.com>
2017-10-28 23:43:42 +0100

    Cleaned up component, fixed css and wrote tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import icons from 'glyphicons';
 
 class App extends Component {
     static defaultProps = {
@@ -14,23 +13,23 @@ class App extends Component {
 
     render() {
         return (
-            <div className="list-item-card">
-                <button className="btn btn-default btn-recipe-list-item-open-recipe">
-                    <i className="glyphicon glyphicon-chevron-right" />
-                </button>
-                <div className="recipe-list-item-image-container">
-                    <img src={this.props.image} className="img-responsive recipe-list-item-image" alt={this.props.title}/>
-                </div>
-                <div className="recipe-list-item-text-container">
-                    <div className="recipe-list-item-title">{this.props.title}</div>
-                    <div>
-                        You have {`${this.props.missedIngredientCount === 0 ? "all" : ""}`} {this.props.usedIngredientCount} ingredients
+            <div className="list-item-card recipe-item-card">
+                <div className="recipe-item-content-container">
+                    <div className="recipe-list-item-image-container">
+                        <img src={this.props.image} className="img-responsive recipe-list-item-image" alt={this.props.title}/>
                     </div>
-                    {
-                        this.props.missedIngredientCount > 0 &&
-                        <div>There's {this.props.missedIngredientCount} missing though</div>
-                    }
+                    <div className="recipe-list-item-text-container">
+                        <div className="recipe-list-item-title">{this.props.title}</div>
+                        <div className="recipe-list-item-used-ingredients">
+                            You have {`${this.props.missedIngredientCount === 0 ? "fuck" : ""}`} {this.props.usedIngredientCount} ingredients
+                        </div>
+                        {
+                            this.props.missedIngredientCount > 0 &&
+                            <div className="recipe-list-item-missing-ingredients">There's {this.props.missedIngredientCount} missing though</div>
+                        }
+                    </div>
                 </div>
+                <i className="recipe-list-item-chevron glyphicon glyphicon-chevron-right" />
             </div>
         );
     }
