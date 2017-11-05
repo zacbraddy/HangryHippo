21dee28df8c36f2f012fac53ac5ab0bff003fec1
Zac Braddy <zacharybraddy@gmail.com>
2017-11-05 21:22:36 +0000

    Reeled back the code to conform to where we'll get to with u4-m2

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
 
-const getPropValue = wrapper => elementType => classToSearchFor => (
-  wrapper.find(elementType).findWhere(e => e.props().className && e.props().className.indexOf(classToSearchFor) !== -1)
-);
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
@@ -16,51 +12,12 @@ describe('Recipe List Item tests', () => {
   it('renders a div with the className prop set to be an item card', () => {
     const wrapper = shallow(<App />);
 
-    expect(getPropValue(wrapper)('div')('list-item-card').length).toBe(1);
+    expect(wrapper.find('div').findWhere(e => e.props().className === 'list-item-card').length).toBe(1);
   });
 
-  it('should display the image supplied by the props', () => {
-    const testAlt = `I'm Batman`;
-    const testImage = 'http://www.vectortemplates.com/raster/batman-logo-big.gif';
-    const wrapper = shallow(<App title={testAlt} image={testImage} />);
+  it('renders a stringified version of props in the div', () => {
+    const wrapper = shallow(<App />);
 
-    expect(getPropValue(wrapper)('img')('recipe-list-item-image').props().alt).toEqual(testAlt);
-    expect(getPropValue(wrapper)('img')('recipe-list-item-image').props().src).toEqual(testImage);
-  });
-
-  it('should display a title', () => {
-    const testTitle = `I'm Batman`;
-    const wrapper = shallow(<App title={testTitle} />);
-
-    expect(getPropValue(wrapper)('div')('recipe-list-item-title').text()).toEqual(testTitle);
-  });
-
-  it('should show the number of ingredients used', () => {
-    const testUsedIngredientsCount = 1;
-    const wrapper = shallow(<App usedIngredientCount={testUsedIngredientsCount} />);
-
-    expect(getPropValue(wrapper)('div')('recipe-list-item-used-ingredients').text().indexOf(`${testUsedIngredientsCount}`)).not.toEqual('-1');
-  });
-
-  it('should prefer to say we use all the ingredients instead of saying the number if we use them all', () => {
-    const testUsedIngredientsCount = 1;
-    const testMissingIngredientsCount = 0;
-    const wrapper = shallow(<App usedIngredientCount={testUsedIngredientsCount} missedIngredientCount={testMissingIngredientsCount}/>);
-
-    expect(getPropValue(wrapper)('div')('recipe-list-item-used-ingredients').text().indexOf('all')).not.toEqual('-1');
-  });
-
-  it('should show a missing ingredient count if we have missing ingredients', () => {
-    const testMissingIngredientsCount = 1;
-    const wrapper = shallow(<App missedIngredientCount={testMissingIngredientsCount} />);
-
-    expect(getPropValue(wrapper)('div')('recipe-list-item-missing-ingredients').text().indexOf(`${testMissingIngredientsCount}`)).not.toEqual('-1');
-  });
-
-  it('should not show a missing ingredient count if there are no missing ingredients', () => {
-      const testMissingIngredientsCount = 0;
-      const wrapper = shallow(<App missedIngredientCount={testMissingIngredientsCount} />);
-
-      expect(getPropValue(wrapper)('div')('recipe-list-item-missing-ingredients').length).toEqual(0);
+    expect(wrapper.find('div').findWhere(e => e.props().className === 'list-item-card').text()).toBe(JSON.stringify(App.defaultProps));
   });
 });
