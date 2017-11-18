6ed7e73a955c566d2d503506741726c5df123a64
Zac Braddy <zacharybraddy@gmail.com>
2017-11-18 15:42:00 +0000

    Updated application structure to something more scalable for module 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import RecipeItem from './recipeItem/components/RecipeItem';
 
 class App extends Component {
     static defaultProps = {
@@ -13,24 +14,7 @@ class App extends Component {
 
     render() {
         return (
-            <div className="list-item-card recipe-item-card">
-                <div className="recipe-item-content-container">
-                    <div className="recipe-list-item-image-container">
-                        <img src={this.props.image} className="img-responsive recipe-list-item-image" alt={this.props.title}/>
-                    </div>
-                    <div className="recipe-list-item-text-container">
-                        <div className="recipe-list-item-title">{this.props.title}</div>
-                        <div className="recipe-list-item-used-ingredients">
-                            You have {`${this.props.missedIngredientCount === 0 ? "fuck" : ""}`} {this.props.usedIngredientCount} ingredients
-                        </div>
-                        {
-                            this.props.missedIngredientCount > 0 &&
-                            <div className="recipe-list-item-missing-ingredients">There's {this.props.missedIngredientCount} missing though</div>
-                        }
-                    </div>
-                </div>
-                <i className="recipe-list-item-chevron glyphicon glyphicon-chevron-right" />
-            </div>
+          <RecipeItem {...this.props} />
         );
     }
 }
---
+++ src/recipeItem/__tests__/RecipeItem.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import RecipeItem from '../components/RecipeItem';
+
+const getElement = wrapper => elementType => classToSearchFor => (
+  wrapper.find(elementType).findWhere(e => e.props().className && e.props().className.indexOf(classToSearchFor) !== -1)
+);
+
+describe('Recipe List Item', () => {
+  it('renders a div with the className prop set to be an item card', () => {
+    const wrapper = shallow(<RecipeItem />);
+
+    expect(getElement(wrapper)('div')('list-item-card').length).toBe(1);
+  });
+
+  it('should display the image supplied by the props', () => {
+    const testAlt = `I'm Batman`;
+    const testImage = 'http://www.vectortemplates.com/raster/batman-logo-big.gif';
+    const wrapper = shallow(<RecipeItem title={testAlt} image={testImage} />);
+
+    expect(getElement(wrapper)('img')('recipe-list-item-image').props().alt).toEqual(testAlt);
+    expect(getElement(wrapper)('img')('recipe-list-item-image').props().src).toEqual(testImage);
+  });
+
+  it('should display a title', () => {
+    const testTitle = `I'm Batman`;
+    const wrapper = shallow(<RecipeItem title={testTitle} />);
+
+    expect(getElement(wrapper)('div')('recipe-list-item-title').text()).toEqual(testTitle);
+  });
+
+  it('should show the number of ingredients used', () => {
+    const testUsedIngredientsCount = 1;
+    const wrapper = shallow(<RecipeItem usedIngredientCount={testUsedIngredientsCount} />);
+
+    expect(getElement(wrapper)('div')('recipe-list-item-used-ingredients').text().indexOf(`${testUsedIngredientsCount}`)).not.toEqual(-1);
+  });
+
+  it('should prefer to say we use all the ingredients instead of saying the number if we use them all', () => {
+    const testUsedIngredientsCount = 1;
+    const testMissingIngredientsCount = 0;
+    const wrapper = shallow(<RecipeItem usedIngredientCount={testUsedIngredientsCount} missedIngredientCount={testMissingIngredientsCount}/>);
+
+    expect(getElement(wrapper)('div')('recipe-list-item-used-ingredients').text().indexOf('all')).not.toEqual(-1);
+  });
+
+  it('should show a missing ingredient count if we have missing ingredients', () => {
+    const testMissingIngredientsCount = 1;
+    const wrapper = shallow(<RecipeItem missedIngredientCount={testMissingIngredientsCount} />);
+
+    expect(getElement(wrapper)('div')('recipe-list-item-missing-ingredients').text().indexOf(`${testMissingIngredientsCount}`)).not.toEqual('-1');
+  });
+
+  it('should not show a missing ingredient count if there are no missing ingredients', () => {
+      const testMissingIngredientsCount = 0;
+      const wrapper = shallow(<RecipeItem missedIngredientCount={testMissingIngredientsCount} />);
+
+      expect(getElement(wrapper)('div')('recipe-list-item-missing-ingredients').length).toEqual(0);
+  });
+});
