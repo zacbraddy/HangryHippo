e05f675635bd7299d1b79b845ab2c6fbfafdb79a
Zac Braddy <zac.braddy@buyingbutler.com>
2018-01-22 01:43:55 +0000

    Added proptypes to recipe item list

--- src/recipeItemList/components/RecipeItemList.jsx
+++ src/recipeItemList/components/RecipeItemList.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import RecipeItem from "../../recipeItem/components/RecipeItem";
-import "../styles/recipeItemList.css";
+import PropTypes from 'prop-types';
+import RecipeItem from '../../recipeItem/components/RecipeItem';
+import '../styles/recipeItemList.css';
 
 export default class RecipeItemList extends Component {
+  static propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      image: PropTypes.string,
+      usedIngredientCount: PropTypes.number,
+      missedIngredientCount: PropTypes.number,
+      likes: PropTypes.number,
+    }))
+  };
+
   render() {
     return (
       <div className="recipe-item-list">
