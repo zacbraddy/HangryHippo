959512bd8a8e1cd0fe60949c36f33fa5376d450e
Zac Braddy <zacharybraddy@gmail.com>
2018-06-11 20:59:34 +0100

    Rollback to make the begin branch

--- src/App/components/App.jsx
+++ src/App/components/App.jsx
@@ -5,7 +5,18 @@ import IngredientsList from '../../ingredientsList/components/IngredientsList';
 import RecipeItemList from '../../recipeItemList/components/RecipeItemList';
 import Recipe from '../../recipe/components/Recipe';
 
-import SpoonacularApi from '../../communications/spoonacularApi';
+const recipeSpec = {
+    id: 641803,
+    title: "Easy & Delish! ~ Apple Crumble",
+    image: "https://spoonacular.com/recipeImages/Easy---Delish--Apple-Crumble-641803.jpg",
+    usedIngredientCount: 3,
+    missedIngredientCount: 4,
+    likes: 1
+  };
+
+export const recipeInstructions = {
+  "instructions": "<ol><li>Preheat Oven 350 degrees:</li><li>Combine sliced apples, lemon zest, dash of ground cloves and sugar in a bowl and toss. Place in a deep dish buttered baking dish.</li><li>In a smaller bowl combine the flour, sugar, brown sugar and butter. Mix this together with your fingers until it becomes crumbly. Place this mixture on top of the apples.</li><li>Bake about 40-45 minutes, until the topping gets a little golden color.</li></ol>",
+};
 
 class App extends Component {
   state = {
@@ -43,29 +54,33 @@ class App extends Component {
     });
   };
 
-  doSearch = async () => {
+  doSearch = () => {
     this.setState({
       canSearch: false,
     });
 
-    await SpoonacularApi
-      .getRecipes(this.state.ingredients)
-      .then(newRecipes => {
-        this.setState({
-          recipes: newRecipes,
-          canSearch: true,
-        });
-      });
+    return new Promise((resolve, reject) => {
+      const newRecipeListLength = Math.floor((Math.random() * 5) + 1);
+      const newRecipeList = new Array(newRecipeListLength);
+      newRecipeList.fill(recipeSpec);
+      setTimeout(resolve, 2000, newRecipeList);
+    }).then(newRecipes => {
+      this.setState({
+        recipes: newRecipes,
+        canSearch: true,
+      })
+    });
   };
 
-  showRecipe = async id => {
+  showRecipe = id => {
     this.setState({
       canSearch: false,
       isShowingRecipe: true,
     });
 
-    SpoonacularApi.getRecipeById(id)
-    .then(instructions => {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, 2000, recipeInstructions.instructions);
+    }).then(instructions => {
       this.setState({
         instructions,
       });
